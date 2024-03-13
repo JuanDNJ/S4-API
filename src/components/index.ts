6a716b99f88011daea172f3c9c7953cfad478918
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-03-13 23:23:39 +0100

    Maquetacion final

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,10 @@
 import { Acudit } from "../types";
 
 export const AcuditComponent = (acudit: Acudit) =>  /*html*/(`
-    <p class="content-text flex-1 flex items-center text-xl text-center p-4 ">${acudit.joke}</p>
-    <div class="scores flex justify-center gap-4 p-4 row-span-1">
+    <p class="content-text size-full flex items-center text-2xl text-center text-white px-6">
+        <span>${acudit.joke}</span>
+    </p>
+    <div class="scores flex md:flex-1 justify-center  gap-4 p-4">
         <button type="button" class="score py-2 px-4 text-4xl" data-score="1">☹️</button>
         <button type="button" class="score py-2 px-4 text-4xl" data-score="2">🙂</button>
         <button type="button" class="score py-2 px-4 text-4xl" data-score="3">🤣</button>
@@ -16,7 +18,7 @@ export const CloudComponent = (dataCloud: any) => {
        
             <div class="cloud-image flex items-center justify-between gap-2 text-sm font-bold">
                 <img src="./cloud/${dataCloud.img}.svg" width="32" height="32" alt="Image Cloud" />
-                <strong>${dataCloud.temperature}</strong>
+                <strong>${dataCloud.temperature}ºC</strong>
             </div>
             <div class="flex items-center">
                 <strong class="city text-sm"><span class="text-sm">en</span> ${dataCloud.city}, ${dataCloud.region === "Catalonia" && "Catalunya"}</strong>
